d07d7bd872dd3506f6dff90f5a03441ab4e9868f
Mark Mesander <mark_mesander@hotmail.com>
2023-08-30 13:26:26 +0200

    complete assignment

---
+++ src/main/java/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    String carBrand;
+    String carType;
+    String carBuildYear;
+    String carColor;
+    String carCilinder;
+
+    public Car(String carBrand, String carType, String carBuildYear, String carColor, String carCilinder) {
+        this.carBrand = carBrand;
+        this.carType = carType;
+        this.carBuildYear = carBuildYear;
+        this.carColor = carColor;
+        this.carCilinder = carCilinder;
+    }
+
+    public void message() {
+        System.out.println("Deze " + carBrand + " " + carType + " is " + carColor);
+    }
+}
